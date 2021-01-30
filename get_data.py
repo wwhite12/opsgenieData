78d0c8788584b92a6aa91b8e65fff3f220e34bea
William White <willcwhite@gmail.com>
2021-01-29 19:01:29 -0500

    move logic into separate functions

--- get_data.py
+++ get_data.py
@@ -1,47 +1,54 @@
-import requests
+"""Script to call the OpsGenie Logs API to retrieve JSON data"""
 import json
 import re
+import requests
 
-myregex='.+\-.+\-.+\-.+\-.+\-.+'
-starting_file = '/path/to/starting/point/file'
-dates_file = open(starting_file,'r')
-start_date = dates_file.read()
+def api_call(start_date, starting_file):
+    """Call OpsGenie API, parse data, output data to external folder"""
+    while True:
+        call_api = f"https://api.opsgenie.com/v2/logs/list/{str(start_date)}?limit=5"
+        headers = {'Authorization':'GenieKey xxxxxxxxxxxxxxxxxxxx'}
+        list_of_filenames_url = requests.get(call_api,headers=headers)
+        json_filenames = list_of_filenames_url.json()
+        base_url = 'https://api.opsgenie.com/v2/logs/download/'
+        if json_filenames["marker"]:
+            pass
+        else:
+            global final_date
+            final_date = start_date
+            new_date = open(starting_file,'w')
+            new_date.write(final_date)
+            new_date.close()
+            break
 
-while re.search(myregex,start_date):
+        start_date = json_filenames["marker"]
 
-    initial_call = f"https://api.opsgenie.com/v2/logs/list/{str(start_date)}?limit=5"
-    headers = {'Authorization':'GenieKey xxxxxxxxxxxxxxxxxxxx'}
-    list_of_filenames_url = requests.get(api_call,headers=headers)
-    json_filenames = list_of_filenames_url.json()
-    base_url = 'https://api.opsgenie.com/v2/logs/download/'
-    
-    if json_filenames["marker"]:
-        pass
-        ##print("Marker is "+json_filenames["marker"])
-    else:
-        global final_date
-        final_date = start_date
-        new_date = open(starting_file,'w')
-        new_date.write(final_date)
-        break
-   
-    start_date = json_filenames["marker"]
+        for data in json_filenames["data"]:
+            file_name = data["filename"] 
+            file_endpoint = f"{base_url}{str(file_name)}"
+            call_file_endpoint = requests.get(file_endpoint,headers=headers)
+            result_from_endpoint = requests.get(call_file_endpoint.text)
+            #print(result_from_endpoint.text)#THIS IS IT
+            output_path = '/insert/path/to/output/folder/'
+            output_file_name = output_path+file_name
+            x = open(output_file_name,"w")
+            x.write(result_from_endpoint.text)
+            x.close()
 
-    for data in json_filenames["data"]:
-        file_name = data["filename"] 
-        file_endpoint = f"{base_url}{str(file_name)}"
-        call_file_endpoint = requests.get(file_endpoint,headers=headers)
-        result_from_endpoint = requests.get(endpoint.text)
-        #print(result_from_endpoint.text)#THIS IS IT
-        output_path = '/insert/path/to/output/folder/'
-        output_file_name = output_path+file_name
-        x = open(output_file_name,"w")
-        x.write(result_from_endpoint.text)
-        x.close()
-    
-else:
-    print("Error: Invalid Start Date. Check Script")
-    
 
-dates_file.close()
-new_date.close()
+def check_date_format(date_format, current_start_date, file_path):
+    """Check to make sure OpsGenie's date format has not changed"""
+    try:
+        re.search(date_format, current_start_date)
+        api_call(start_date=current_start_date, starting_file=file_path)
+    except:
+        raise Exception("Date format has changed, check OpsGenie Logs API doc page for new format")
+
+def main():
+    """starting point, declare variables, call other functions"""
+    myregex='.+\-.+\-.+\-.+\-.+\-.+'  # pylint: disable=anomalous-backslash-in-string
+    starting_file = '/path/to/starting/point/file'
+    dates_file = open(starting_file,'r')
+    start_date = dates_file.read()
+    check_date_format(date_format=myregex, current_start_date=start_date, file_path=starting_file)
+    dates_file.close()
